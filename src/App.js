c0f4cc53cf0dcb2e9c06a03bcab1a8c5fcc365dc
VermaSankalp <vermasankalp1@gmail.com>
2022-11-10 00:11:00 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -2,28 +2,14 @@ import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SolrConnector from 'react-solr-connector';
+import SearchBar from './Components/seachBar';
 
 const App = () => {
   const [searchParams, setSearchParams] = useState(null);
 
   return (
     <SolrConnector searchParams={searchParams}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <SearchBar />
     </SolrConnector>
   )
 }
