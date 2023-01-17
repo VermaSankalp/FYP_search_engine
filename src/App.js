198a5a4910b14b898cf0e95f288458df92c48aaf
VermaSankalp <vermasankalp1@gmail.com>
2023-01-17 17:41:47 +0800

    back to basic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React, {useState} from 'react';
 import './App.css';
-// import SolrConnector from 'react-solr-connector';
-// import SearchBar from './Components/SolrQuery/SearchBar';
-// import DisplayResults from './Components/DisplayResults/DisplayResults';
-import Ting from './axios';
-import UserInput from './Components/userInput';
+import SolrConnector from 'react-solr-connector';
+import SearchBar from './Components/SolrQuery/SearchBar';
+import DisplayResults from './Components/DisplayResults/DisplayResults';
+// import Ting from './axios';
+// import UserInput from './Components/userInput';
 
 const App = () => {
-  // const [searchParams, setSearchParams] = useState(null);
-  const [query, setQuery] = useState("russia");
-  const [fetchFields, setFetchFields] = useState("id");
+  const [searchParams, setSearchParams] = useState(null);
+  // const [query, setQuery] = useState("russia");
+  // const [fetchFields, setFetchFields] = useState("id");
 
-  // const doSearch = (searchParams) => {
-  //     setSearchParams(searchParams);
-  // }
+  const doSearch = (searchParams) => {
+      setSearchParams(searchParams);
+  }
 
   return (
-    // <SolrConnector searchParams={searchParams}>
-    //   <SearchBar doSearch={doSearch}/>
-    //   <DisplayResults />
-    // </SolrConnector>
-    <div style={{marginLeft: '30px'}}>
-      <UserInput query={query} setQuery={setQuery} fetchFields={fetchFields} setFetchFields={setFetchFields} />
-      <Ting query={query} fetchFields={fetchFields} />
-    </div>
+    <SolrConnector searchParams={searchParams}>
+      <SearchBar doSearch={doSearch}/>
+      <DisplayResults />
+    </SolrConnector>
+    // <div style={{marginLeft: '30px'}}>
+    //   <UserInput query={query} setQuery={setQuery} fetchFields={fetchFields} setFetchFields={setFetchFields} />
+    //   <Ting query={query} fetchFields={fetchFields} />
+    // </div>
   )
 }
 
