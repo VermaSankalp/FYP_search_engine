b76f38ce0d37c8443ac3f9c169e7e8ee526e9acb
Wilson <chiuwilson09@gmail.com>
2023-01-22 01:03:35 +0800

    button and input components

---
+++ src/common/Components/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type ButtonProps = {
+    children : React.ReactNode; 
+    buttonCallback : Function;
+    customStyles : string[]; 
+}
+export const Button = ({children, buttonCallback, customStyles} : ButtonProps) : JSX.Element=> {
+    const baseClass = ["py-2", "text-white", "bg-black", "rounded-full"]
+    const finalClass = baseClass.concat(customStyles); 
+    return (
+        <button className={finalClass.join(" ")} onClick={()=>buttonCallback()}>
+            {children}
+        </button>
+    )
+}
--- src/pages/SearchHome.js
+++ src/pages/SearchHome.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+import { Button } from "../common/Components/Button.tsx";
+import { InputField } from "../common/Components/InputField.tsx";
 const SearchHome = () => {
+    const [query, setQuery] = useState("")
     return (
-        <div>
-            Search Home Page
+        <div className="bg-white">
+            <div className="flex flex-col gap-4 align-middle item-center">
+                <div className="text-4xl">Financial News Search</div>
+                <InputField customStyles={["py-4", "w-[600px]", "text-2xl"]} inputCallback={(query) => setQuery(query)}/>
+                <Button 
+                    buttonCallback={()=>console.log(`Search ${query}`)}
+                    customStyles={["w-[200px]"]}
+                >
+                    Search
+                </Button>
+            </div>
+            <div>
+                {query}
+            </div>
         </div>
     )
 }
