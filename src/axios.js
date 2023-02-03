817e81d607e1de5b5a664dfe04185ffa397bbcaf
Wilson <chwchiu@connect.ust.hk>
2023-02-03 12:31:28 +0800

    eslint errors

--- src/axios.js
+++ src/axios.js
@@ -1,36 +1,36 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
-import DisplayResults from "./common/Components/DisplayResults.tsx";
+import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import DisplayResults from "./common/Components/DisplayResults.tsx";
 
 const Axios = (props) => {
-    const [bigMan, setBigMan] = useState();
-    const [solrSearchUrl, setSolrSearchUrl] = useState("");
+    // const [bigMan, setBigMan] = useState();
+    // const [solrSearchUrl, setSolrSearchUrl] = useState("");
 
-   useEffect(() => {
-       setSolrSearchUrl("http://localhost:8983/solr/fyp_documents/select")
-       axios.get(solrSearchUrl, {
-           params: {
-               "fl": props.fetchFields,
-               "q": props.query,
-               "indent": true,
-               "q.op": "OR",
-               "rows": 5
-           }
-       })
-       .then(res => {
-           const bigManTing = res.data.response.docs;
-           setBigMan(bigManTing);
-        //    console.log(bigManTing);
-       })
-   }, [props.fetchFields, props.query, solrSearchUrl])
+    // useEffect(() => {
+    //     setSolrSearchUrl("http://localhost:8983/solr/fyp_documents/select")
+    //     axios.get(solrSearchUrl, {
+    //         params: {
+    //             "fl": props.fetchFields,
+    //             "q": props.query,
+    //             "indent": true,
+    //             "q.op": "OR",
+    //             "rows": 5
+    //         }
+    //     })
+    //         .then(res => {
+    //             const bigManTing = res.data.response.docs;
+    //             //    setBigMan(bigManTing);
+    //             //    console.log(bigManTing);
+    //         })
+    // }, [props.fetchFields, props.query, solrSearchUrl])
 
-   return (
+    return (
         <div>
             {/* {bigMan !== undefined ? 
                 <DisplayResults results={bigMan} /> : "Piss off"} */}
             {props.query}
         </div>
-   )
+    )
 }
 
 export default Axios;
